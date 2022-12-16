dd1f8ef858c145ad9a84ef0d9eef239c85de7029
MdSharfarazHossain <sharfaraz.hossain@norhtsouth.edu>
2022-12-16 21:14:11 +0600

    login and logout

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, flash, jsonify, redirect, render_template, request, session, send_file
 from flask_session import Session
+import sys
 
 from flask_sqlalchemy import SQLAlchemy 
-
+import logging
 
 import sqlite3, os, time, fnmatch
 from tempfile import mkdtemp
@@ -38,7 +39,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 @app.route("/Home")
 def index():
     
-    rows = db.execute("SELECT * FROM accounts")
+    rows = db.execute("SELECT * FROM accounts WHERE Username = :username", username="Daniel K. Pemberton")
     return render_template("landing.html", rows=rows)
 
 @app.route("/AboutUs")
@@ -53,9 +54,57 @@ def Complaints():
 def FAQ():
     return render_template("FAQ.html")
 
-@app.route("/Login")
-def login():
-    return render_template("Login.html")
+
+
+
+
+
+@app.route("/Login", methods=["GET", "POST"])
+def login(warning = 0):
+    print(request.method, file=sys.stderr)
+    if request.method == "POST":
+        # Verify that the input fields are not empty
+        if not request.form.get("loginUser"):
+            return render_template("Login.html", warning = 1)
+        if not request.form.get("loginPassword"):
+            return render_template("Login.html",warning = 1)
+        
+        # Store the user input
+        username = request.form.get("loginUser")
+        password = request.form.get("loginPassword")
+        
+
+        # Query the database for user credentials
+        rows = db.execute("SELECT * FROM accounts WHERE Username = :username", username=username)
+
+        # Ensure the user exists and their password is correct
+        if len(rows) != 1 or not rows[0]["Password1"] == password:
+            return render_template("Login.html",warning = 1)
+
+        # Hold the user's session
+        session["user_id"] = rows[0]["accId"]
+
+        # Redirect user to home page
+        return redirect("/")
+    else:
+        return render_template("Login.html", warning=0)
+
+
+@app.route("/Logout")
+def logout():
+    # Clear the current session to log the user out, and redirect them to the homepage
+    session.clear()
+    return redirect("/")
+
+
+
+
+
+
+
+
+
+
 
 @app.route("/Metrocard")
 def metrocard():
