2e5c22fb4e5595973c15fd1b18cbb9d3f429ebd1
MdSharfarazHossain <sharfaraz.hossain@norhtsouth.edu>
2022-12-17 01:30:14 +0600

    login logout and complaint

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
-#app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/metroraileticket.sql"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:password123@localhost/metroraileticket.sql"
 #db = SQLAlchemy(app)
 
 
@@ -37,22 +37,37 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 @app.route("/Home")
-def index():
-    
-    rows = db.execute("SELECT * FROM accounts WHERE Username = :username", username="Daniel K. Pemberton")
+def index():    
     return render_template("landing.html", rows=rows)
 
 @app.route("/AboutUs")
 def AboutUs():
     return render_template("AboutUs.html")
 
-@app.route("/Complaints")
+@app.route("/Complaints", methods = ["POST", "GET"])
 def Complaints():
-    return render_template("Complaints.html")
+    if request.method == "POST":
+        # Verify that the input fields are not empty
+        if not request.form.get("subject"):
+            return render_template("Complaints.html", submission=True, warning = 1)
+        if not request.form.get("description"):
+                return render_template("Complaints.html", submission=True, warning = 1)
+        
+        compSub = request.form.get("subject")
+        compDesc = request.form.get("description")
+        
+        print(session["user_id"], file=sys.stderr)
+        db.execute("INSERT INTO complaintTab (accId, ComplaintText, complaintSub) VALUES (:userID, :desc, :sub)", userID=int(session["user_id"]), desc = compDesc, sub = compSub)
+        
+        return render_template("Complaints.html", submission=True, warning = 0)
+    else:
+        return render_template("Complaints.html", submission=False, warning = 0)
+    
 
 @app.route("/FAQ")
 def FAQ():
-    return render_template("FAQ.html")
+    faqList = db.execute("SELECT * FROM faqList")
+    return render_template("FAQ.html", faqList=faqList)
 
 
 
@@ -61,7 +76,6 @@ def FAQ():
 
 @app.route("/Login", methods=["GET", "POST"])
 def login(warning = 0):
-    print(request.method, file=sys.stderr)
     if request.method == "POST":
         # Verify that the input fields are not empty
         if not request.form.get("loginUser"):
@@ -82,7 +96,7 @@ def login(warning = 0):
             return render_template("Login.html",warning = 1)
 
         # Hold the user's session
-        session["user_id"] = rows[0]["accId"]
+        session["user_id"] = int(rows[0]["accId"])
 
         # Redirect user to home page
         return redirect("/")
